960e9768efff289bc4396cb1121098272363031d
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-22 14:35:28 +0100

    Pulsante ripristina

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ var app = new Vue({
     methods: {
         getToDo: function() {
             var input = document.getElementById('to-do-input');
-            this.toDos.push(input.value);
-            input.value = '';
+            if (input.value != 0) {
+                this.toDos.push(input.value);
+                input.value = '';
+            }
         },
         removeToDo: function(index) {
             var cancelled = this.toDos.splice(index,1);
@@ -26,6 +28,12 @@ var app = new Vue({
         undoDoneToDo: function(index) {
             var done = this.doneToDos.splice(index,1);
             this.toDos.push(done[0]);
+        },
+        restoreCancelledToDos: function() {
+            this.cancelledToDos.forEach((element,index) => {
+                this.toDos.push(element);
+            });
+            this.cancelledToDos = [];
         }
     }
 
