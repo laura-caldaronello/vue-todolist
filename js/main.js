e14d7898edc36e9a28c86099e647179acec10283
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-22 13:37:13 +0100

    Passo a caso

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 var app = new Vue({
     el: '#root',
     data: {
-        toDos: ['ciao']
+        toDos: ['ciao'],
+        cancelledToDos: [],
+        doneToDos: []
     },
     methods: {
         getToDo: function() {
-            var input = document.getElementById('to-do');
+            var input = document.getElementById('to-do-input');
             this.toDos.push(input.value);
+            input.value = '';
         },
         removeToDo: function(index) {
-            this.toDos.splice(index,1);
+            var cancelled = this.toDos.splice(index,1);
+            this.cancelledToDos.push(cancelled[0]);
+        },
+        doneToDo: function(index) {
+            var done = this.toDos.splice(index,1);
+            this.doneToDos.push(done[0]);
         }
     }
 
